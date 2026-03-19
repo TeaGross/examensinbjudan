f29dbf75cc3881e984a3bb68a882b3e5e3af92d9
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2026-03-19 22:03:31 +0100

    ✨ feat: add invitation card component

---
+++ examensinbjudan/src/components/InvitationCard/InvitationCard.tsx
@@ -0,0 +1,9 @@
+import kuvert from "../../assets/kuvert.png"
+import "./InvitationCard.css"
+
+export const InvitationCard = () => {
+
+    return (
+        <img src={kuvert} alt="ett kuvert" className="invitation-card"/>
+    )
+}
